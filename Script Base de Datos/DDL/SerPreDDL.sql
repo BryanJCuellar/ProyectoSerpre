55d7fe3057f9259069f040c3a1df6bf392f241ff
BryanJCuellar <bryanb194@yahoo.com>
2020-04-22 01:19:44 -0600

    Update

--- Script Base de Datos/DDL/SerPreDDL.sql
+++ Script Base de Datos/DDL/SerPreDDL.sql
@@ -19,10 +19,18 @@ CREATE TABLE `id12846177_serprehn`.`Usuarios_Registrados` (
   `Nombre_Usuario` VARCHAR(45) NOT NULL,
   `Email` VARCHAR(45) NOT NULL,
   `Password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID_Usuario`),
+  CONSTRAINT `PK_Usuarios_Registrados` PRIMARY KEY (`ID_Usuario`),
   CONSTRAINT `CHK_GENERO` CHECK(`Genero` IN('M','F','N/A'))
-  )
+ )
 
+-- -----------------------------------------------------
+-- Table `id12846177_serprehn`.`Categoria_Servicio`
+-- -----------------------------------------------------
+CREATE TABLE `id12846177_serprehn`.`Categoria_Servicio` (
+	`ID_Categoria_Servicio` INT NOT NULL,
+	`Nombre_Categoria` VARCHAR(90) NOT NULL,
+	CONSTRAINT `PK_Categoria_Servicio` PRIMARY KEY (`ID_Categoria_Servicio`)
+)
 
 -- -----------------------------------------------------
 -- Table `id12846177_serprehn`.`Servicios_Publicados`
@@ -31,20 +39,26 @@ CREATE TABLE `id12846177_serprehn`.`Servicios_Publicados` (
   `ID_Servicio` INT NOT NULL,
   `Imagen` BLOB NULL,
   `Nombre_Servicio` VARCHAR(45) NOT NULL,
-  `Categoria` VARCHAR(90) NOT NULL,
   `Fecha_Publicacion` DATE NOT NULL,
   `Hora_Publicacion` TIME NOT NULL,
-  `Resumen_Descripcion` VARCHAR(45) NOT NULL,
-  `Detalle_Descripcion` VARCHAR(100) NOT NULL,
-  `Precio` FLOAT NOT NULL,
-  `Moneda` VARCHAR(20) NOT NULL,
+  `Resumen_Descripcion` VARCHAR(45) NULL,
+  `Detalle_Descripcion` VARCHAR(100) NULL,
+  `Precio` FLOAT NULL,
+  `Moneda` VARCHAR(20) NULL,
   `ID_Usuario_Publicador` INT NOT NULL,
-  PRIMARY KEY (`ID_Servicio`),
+  `ID_Categoria_Servicio` INT NOT NULL,
+  CONSTRAINT `PK_Servicios_Publicados` PRIMARY KEY (`ID_Servicio`),
   CONSTRAINT `CHK_MONEDA` CHECK(`Moneda` IN('Lempira','Dolar','Euro')),
-  INDEX `fk_Servicios_Publicados_Usuarios_Registrados_idx` (`ID_Usuario_Publicador` ASC),
-  CONSTRAINT `fk_Servicios_Publicados_Usuarios_Registrados`
+  INDEX `fk_Servicios_Publicados_Usuarios_Registrados1_idx` (`ID_Usuario_Publicador` ASC),
+  INDEX `fk_Servicios_Publicados_Categoria_Servicio1_idx` (`ID_Categoria_Servicio` ASC),
+  CONSTRAINT `fk_Servicios_Publicados_Usuarios_Registrados1`
     FOREIGN KEY (`ID_Usuario_Publicador`)
     REFERENCES `id12846177_serprehn`.`Usuarios_Registrados` (`ID_Usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Servicios_Publicados_Categoria_Servicio1`
+    FOREIGN KEY (`ID_Categoria_Servicio`)
+    REFERENCES `id12846177_serprehn`.`Categoria_Servicio` (`ID_Categoria_Servicio`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
---
+++ Script Base de Datos/DML/insertsCategoriaServicio.sql
@@ -0,0 +1,3 @@
+-- Categoria_Servicio
+INSERT INTO `Categoria_Servicio` (`ID_Categoria_Servicio`,`Nombre_Categoria`) 
+VALUES (1,"Cursos"),(2,"Tutorias"),(3,"Articulos de Segunda Mano"),(4,"Eventos"),(5,"Reparaciones");
