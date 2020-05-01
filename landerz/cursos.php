04cf7acc91e1b2a6b64d93bfc3948ceaa362eda8
hectorriver <48781666+hectorriver@users.noreply.github.com>
2020-05-01 15:08:26 -0600

    Create cursos.php

---
+++ landerz/cursos.php
@@ -0,0 +1,350 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>SerPre HN</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="icon" href="icon/NewLogo.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap" id="inicio">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body">
+      </div>
+    </div>
+   
+    <div class="container d-none d-lg-block">
+      <div class="row">
+        <div class="col-12 text-center mb-4 mt-5">
+         
+            <!--<h1 class="mb-0 site-logo"><a href="index.php" class="text-black h2 mb-0">SerPre HN<span class="text-primary">.</span> </a></h1>-->
+        </div>
+      </div>
+    </div>
+    <header class="site-navbar py-md-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-md-6 col-xl-2  d-block d-lg-none">
+            <h1 class="mb-0 site-logo"><a href="index.php" class="text-black h2 mb-0">SerPre HN<span class="text-primary">.</span> </a></h1>
+          </div>
+
+          <div class="col-12 col-md-10 main-menu">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#inicio" class="nav-link">Inicio</a></li>
+                <li><a href="#servicios" class="nav-link">Servicios</a></li>
+                <li><a href="#sobre-nosotros" class="nav-link">Sobre Nosotros</a></li>
+                <li><a href="#formulario-de-contacto" class="nav-link">Contáctanos</a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 col-md-6 d-inline-block d-lg-none ml-md-0" ><a href="#" class="site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+    
+
+    <div class="site-blocks-cover">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+
+          <div class="col-md-12" style="position: relative;" data-aos="fade-up">
+            
+            <!--<img src="images/LogoSerpre.png" alt="Image" class="img-logo img-absolute img-fluid">-->
+
+            <div class="row mb-4"><!--mb-4-->
+              <div class="col-lg-6 mr-auto">
+                <h1 id="ToStarted">Cursos</h1>
+                <h5>
+                <br>
+                Estamos comprometidos con tigo, ofreciendote cursos a tu medidad con la mejor calidad y profesores certificados 
+            
+
+                </h5>
+                <p class="mb-5">
+                  .</p>
+                
+              </div>
+              <div class="col-lg-6 mr-auto">
+                <img src="images/eventos.jpg" alt="LogoImage" class="img-logo img-fluid">
+              </div>
+              
+              
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>  
+
+
+    <div class="site-section bg-light" id="servicios">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <h2 class="section-title mb-3">Cursos</h2>
+            <h6> Nuestro curso fue creado para ayudar a enriquecer tus conocimientos y darte la mejor experiencia 
+                 para que aprendas de una manera mas facil, apoyandomos en nuestra experiencia y eficiencia
+            
+            <br>
+            <br>
+            Por lo tanto si necesitas informacion dale clic en Llenar Formulario y nosotros nos pondremos en contacto contigo.
+             </h6>
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+        <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+            
+            <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+             <!-- <span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="https://webtematica.com/sites/default/files/styles/790px/public/curso-desarrollo-diseno-web_0.gif?itok=nDNI7czO" 
+              srcset="https://webtematica.com/sites/default/files/styles/790px/public/curso-desarrollo-diseno-web_0.gif?itok=nDNI7czO 1x, https://webtematica.com/sites/default/files/styles/790px/public/curso-desarrollo-diseno-web_0.gif?itok=nDNI7czO 2x" 
+              width="240" 
+              height="135"></div>
+              <div>
+                <h3>Curso de desarrollo de web</h3>
+                <h6> </h6>
+                  </div>
+                  
+                  <span> 50 $ </span>
+                  
+              </a>
+            </div>
+          </div>
+
+              <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+              <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+              <!--<span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="https://trabajarporelmundo.org/wp-content/uploads/2017/09/mejores-cursos-de-ingles-gratuitos.jpg" 
+              srcset="https://trabajarporelmundo.org/wp-content/uploads/2017/09/mejores-cursos-de-ingles-gratuitos.jpg 1x, https://trabajarporelmundo.org/wp-content/uploads/2017/09/mejores-cursos-de-ingles-gratuitos.jpg 2x" 
+              width="240" 
+              height="135"></div>
+              <div>
+              <h3>Curso de ingles </h3>
+                <h6> </h6>
+                
+              </div>
+              <span> 20 $ </span>
+              </a>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+            <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+              <!--<span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="http://www.riial.org/wp-content/uploads/2017/06/dise%C3%B1o-gr%C3%A1fico.png" 
+              srcset="http://www.riial.org/wp-content/uploads/2017/06/dise%C3%B1o-gr%C3%A1fico.png 1x, http://www.riial.org/wp-content/uploads/2017/06/dise%C3%B1o-gr%C3%A1fico.png 2x" 
+              width="240" 
+              height="135"></div>
+
+              
+              <div>
+              <h3>Curso de diseño gráfico </h3>
+                <h6> </h6>
+                 </div>
+                 <span> 25 $ </span>
+              </a>
+            </div>
+          </div>
+
+                       
+
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">  
+            <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+              <!--<span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="https://assets.entrepreneur.com/content/3x2/2000/20171117161507-mkt-digital.jpeg?width=700&crop=2:1" 
+              srcset="https://assets.entrepreneur.com/content/3x2/2000/20171117161507-mkt-digital.jpeg?width=700&crop=2:1 1x, https://assets.entrepreneur.com/content/3x2/2000/20171117161507-mkt-digital.jpeg?width=700&crop=2:1 2x" 
+              width="240" 
+              height="135"></div>
+              <div>
+              <h3>Curso de marketing Digital </h3>
+                </div>
+              <span> 22.50 $ </span>
+              </a>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+            <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+              <!--<span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="http://blog.channeldir.com/wp-content/uploads/2017/07/curso-de-programaci%C3%B3n-desde-cero.jpg" 
+              srcset="http://blog.channeldir.com/wp-content/uploads/2017/07/curso-de-programaci%C3%B3n-desde-cero.jpg 1x, http://blog.channeldir.com/wp-content/uploads/2017/07/curso-de-programaci%C3%B3n-desde-cero.jpg 2x" 
+              width="240"
+              height="135"></div>
+              <div>
+              <h3>Curos de programacion</h3>
+
+              
+              </div>
+              <span> GRATIS </span>
+              </a>
+            </div>
+          </div>
+
+
+
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+          <div class="unit-4 d-block">
+              <a href="#ToStarted" style="color:gray" >
+              <div class="unit-4-icon mb-3">
+               <!-- <span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>-->
+              </div>
+              <div> 
+                <img class="merchandising-course-card--course-image--3G7Kh" 
+              alt="" 
+              src="https://www.neolo.com/blog/wp-content/uploads/2016/12/cursos-aplicaciones-moviles.png" 
+              srcset="https://www.neolo.com/blog/wp-content/uploads/2016/12/cursos-aplicaciones-moviles.png 1x, https://www.neolo.com/blog/wp-content/uploads/2016/12/cursos-aplicaciones-moviles.png 2x" 
+              width="240"
+              height="135"></div>
+              <div>
+              <h3>Curso de desarrollo de aplicaciones móviles </h3>
+
+              
+              </div>
+              <span> 40 $ </span>
+              </a>
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+
+
+
+
+
+
+   
+
+    
+    
+    
+    
+
+
+    <div class="footer py-5 border-top text-center">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12">
+            <p class="mb-0">
+              <a href="#" class="p-3 a-color"><span class="icon-facebook"></span></a>
+              <a href="#" class="p-3 a-color"><span class="icon-twitter"></span></a>
+              <a href="#" class="p-3 a-color"><span class="icon-instagram"></span></a>
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <p class="mb-0 a-color">
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;<script>document.write(new Date().getFullYear());</script>  <i class="" aria-hidden="true"></i> <a href="" target="_blank" ></a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  
+     
+    
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/sendEmail.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  <script src="js/main.js"></script>
+   
+  </body>
+</html>
