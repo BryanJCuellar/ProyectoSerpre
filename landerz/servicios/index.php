d7afbe9fb2589d858c3ee7d392ada6eb51a0541a
BryanJCuellar <bryanb194@yahoo.com>
2020-05-04 17:02:12 -0600

    Update Servicios

--- landerz/servicios/index.php
+++ landerz/servicios/index.php
@@ -515,7 +515,9 @@
             </div>
         </div>
     </div>
-    <!--<script src="../js/jquery.js"></script>
+    <script src="../js/jquery.js"></script>
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
     <script src="../js/sendEmail.js"></script>
     <script src="../js/jquery-migrate-3.0.1.min.js"></script>
     <script src="../js/jquery-ui.js"></script>
@@ -524,12 +526,10 @@
     <script src="../js/jquery.stellar.min.js"></script>
     <script src="../js/jquery.countdown.min.js"></script>
     <script src="../js/bootstrap-datepicker.min.js"></script>
-    <script src="../js/jquery.easing.1.3.js"></script>-->
-    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/jquery.easing.1.3.js"></script>
     <script src="../js/aos.js"></script>
     <script src="../js/jquery.fancybox.min.js"></script>
     <script src="../js/jquery.sticky.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
     <script src="../js/main.js"></script>
     <script src="../js/controladorServicio.js"></script>
 </body>
